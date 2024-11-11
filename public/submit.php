a075c213a8521ee434041349b8b721bc2f78b2a0
Artem Grigoryev <artem.grigoryev@initlab.ru>
2024-11-11 12:55:42 +0300

    add error handler

--- public/submit.php
+++ public/submit.php
@@ -1,12 +1,21 @@
 <?php
 
+if (!file_exists(__DIR__ . '/../vendor/autoload.php')) {
+    header('HTTP/1.1 500 Internal Server Error');
+    echo "Ошибка: файл autoload.php не найден. Пожалуйста, выполните установку зависимостей.";
+    exit;
+}
+
 require_once __DIR__ . '/../vendor/autoload.php';
 
 $config = require __DIR__ . '/../src/config.php';
 
 use App\Controller\LeadController;
 
-$leadController = new LeadController($config);
-echo $leadController->handleRequest();
-
-?>
+try {
+    $leadController = new LeadController($config);
+    echo $leadController->handleRequest();
+} catch (Exception $e) {
+    header('HTTP/1.1 500 Internal Server Error');
+    echo "Произошла ошибка: " . $e->getMessage();
+}
